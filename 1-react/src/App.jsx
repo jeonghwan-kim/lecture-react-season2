0a0e1092ba1924eff70c9740fcaa6deecd5002e8
김정환 <ej88ej@gmail.com>
2023-07-11 07:37:37 +0900

    CartPage

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -1,10 +1,12 @@
+import CartPage from "./pages/CartPage";
 import OrderPage from "./pages/OrderPage";
 import ProductPage from "./pages/ProductPage";
 
 const App = () => (
   <>
     {/* <ProductPage /> */}
-    <OrderPage />
+    {/* <OrderPage /> */}
+    <CartPage />
   </>
 );
 
---
+++ 1-react/src/pages/CartPage/index.jsx
@@ -0,0 +1,28 @@
+import Button from "../../components/Button";
+import Page from "../../components/Page";
+import ProductItem from "../../components/ProductItem";
+import Title from "../../components/Title";
+
+const fakeProduct = {
+  id: "CACDA420",
+  name: "해물 계란 라면",
+  price: 6000,
+  thumbnail: "./images/menu-해물계란라면.jpg",
+};
+
+const CartPage = () => (
+  <div className="CartPage">
+    <Page
+      header={<Title>장바구니</Title>}
+      footer={
+        <Button styleType="brand-solid" block>
+          결제하기
+        </Button>
+      }
+    >
+      <ProductItem product={fakeProduct} />
+    </Page>
+  </div>
+);
+
+export default CartPage;
