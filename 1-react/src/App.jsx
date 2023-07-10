361f9c48c281ece32dbcd8e2d273dc33c69882a0
김정환 <ej88ej@gmail.com>
2023-07-10 12:21:24 +0900

    단일 컴포넌트 App

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -1,3 +1,38 @@
-const App = () => <>1-react</>;
+const App = () => (
+  <div className="ProductPage">
+    <div className="Page">
+      <header>
+        <h1>메뉴목록</h1>
+      </header>
+      <main>
+        <ul>
+          <li>
+            <div className="ProductItem">
+              <div className="description">
+                <h2>고소한 바질 파스타</h2>
+                <div>6,000원</div>
+                <button className="Button brand">주문하기</button>
+              </div>
+              <div className="thumbnail">
+                <img
+                  src="./images/menu-고소한바질파스타.jpg"
+                  alt="고소한바질파스타 6,000원"
+                />
+              </div>
+            </div>
+          </li>
+        </ul>
+      </main>
+      <footer>
+        <nav className="Navbar">
+          <a className="active" href="#">
+            메뉴목록
+          </a>
+          <a href="#">주문내역</a>
+        </nav>
+      </footer>
+    </div>
+  </div>
+);
 
 export default App;
