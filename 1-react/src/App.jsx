d7ad7d90e0057e78ed107d1cefcbc49e0c314de0
김정환 <ej88ej@gmail.com>
2023-07-15 08:40:01 +0900

    Router

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -1,16 +1,18 @@
 import CartPage from "./pages/CartPage";
 import OrderPage from "./pages/OrderPage";
 import ProductPage from "./pages/ProductPage";
+import * as MyRouter from "./lib/MyRouter";
 
-const App = () => {
-  const { pathname } = window.location;
-  return (
-    <>
-      {pathname === "/cart" && <CartPage />}
-      {pathname === "/order" && <OrderPage />}
-      {!["/cart", "/order"].includes(pathname) && <ProductPage />}
-    </>
-  );
-};
+const App = () => (
+  <MyRouter.routerContext.Consumer>
+    {({ path }) => (
+      <MyRouter.Router>
+        {path === "/cart" && <CartPage />}
+        {path === "/order" && <OrderPage />}
+        {!["/cart", "/order"].includes(path) && <ProductPage />}
+      </MyRouter.Router>
+    )}
+  </MyRouter.routerContext.Consumer>
+);
 
 export default App;
--- 1-react/src/lib/MyRouter.jsx
+++ 1-react/src/lib/MyRouter.jsx
@@ -1,7 +1,44 @@
-export const Link = ({ to, ...rest }) => {
-  const handleClick = (e) => {
-    e.preventDefault();
-  };
+import React from "react";
 
-  return <a {...rest} href={to} onClick={handleClick} />;
-};
+export const routerContext = React.createContext({});
+routerContext.displayName = "RouterContext";
+
+export class Router extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      path: window.location.pathname,
+    };
+    this.handleChangePath = this.handleChangePath.bind(this);
+  }
+
+  handleChangePath(path) {
+    this.setState({ path });
+  }
+
+  render() {
+    const contextValue = {
+      path: this.state.path,
+      changePath: this.handleChangePath,
+    };
+
+    return (
+      <routerContext.Provider value={contextValue}>
+        {this.props.children}
+      </routerContext.Provider>
+    );
+  }
+}
+
+export const Link = ({ to, ...rest }) => (
+  <routerContext.Consumer>
+    {({ path, changePath }) => {
+      const handleClick = (e) => {
+        e.preventDefault();
+        if (to !== path) changePath(to);
+      };
+
+      return <a {...rest} href={to} onClick={handleClick} />;
+    }}
+  </routerContext.Consumer>
+);
