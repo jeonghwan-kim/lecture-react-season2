32506953a025f85040896918f83b9b286e35cf48
김정환 <ej88ej@gmail.com>
2023-07-19 07:20:36 +0900

    useEffect

--- 2-hook/src/lib/MyReact.jsx
+++ 2-hook/src/lib/MyReact.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const MyReact = (function MyReact() {
   const memorizedStates = [];
+  const deps = [];
   const isInitialized = [];
+  const cleanups = [];
   let cursor = 0;
 
   function useState(initialValue = "") {
@@ -26,6 +28,38 @@ const MyReact = (function MyReact() {
     return [state, setState];
   }
 
+  function useEffect(effect, nextDeps) {
+    function runDeferedEffect() {
+      function runEffect() {
+        const cleanup = effect();
+        if (cleanup) cleanups[cursor] = cleanup;
+      }
+      const ENOUGH_TIME_TO_RENDER = 1;
+      setTimeout(runEffect, ENOUGH_TIME_TO_RENDER);
+    }
+
+    if (!isInitialized[cursor]) {
+      isInitialized[cursor] = true;
+      deps[cursor] = nextDeps;
+      cursor = cursor + 1;
+      runDeferedEffect();
+      return;
+    }
+
+    const prevDeps = deps[cursor];
+    const depsSame = prevDeps.every(
+      (prevDep, index) => prevDep === nextDeps[index]
+    );
+    if (depsSame) {
+      cursor = cursor + 1;
+      return;
+    }
+
+    deps[cursor] = nextDeps;
+    cursor = cursor + 1;
+    runDeferedEffect();
+  }
+
   // 구현을 위해 이곳만 리액트 훅을 사용한다.
   function useForceUpdate() {
     const [value, setValue] = React.useState(1);
@@ -38,7 +72,21 @@ const MyReact = (function MyReact() {
     };
   }
 
-  return { useState };
+  function resetCursor() {
+    cursor = 0;
+  }
+
+  function cleanupEffects() {
+    cleanups.forEach((cleanup) => typeof cleanup === "function" && cleanup());
+  }
+
+  return {
+    useState,
+    useEffect,
+
+    resetCursor,
+    cleanupEffects,
+  };
 })();
 
 export default MyReact;
