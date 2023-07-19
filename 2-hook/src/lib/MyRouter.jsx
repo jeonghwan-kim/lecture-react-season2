aebd8041ae6b4b204811eb1e3d8714544cf19ee4
김정환 <ej88ej@gmail.com>
2023-07-20 07:52:18 +0900

    Routes, Route, Link

--- 2-hook/src/lib/MyRouter.jsx
+++ 2-hook/src/lib/MyRouter.jsx
@@ -30,3 +30,47 @@ export const Router = () => {
     </routerContext.Provider>
   );
 };
+
+export const Routes = ({ children }) => {
+  const { path } = React.useContext(routerContext);
+
+  // 반환할 리액트 엘리먼트
+  let selectedRoute = null;
+
+  React.Children.forEach(children, (child) => {
+    // 리액트 엘리먼트인지 검사한다
+    if (!React.isValidElement(child)) return;
+
+    // 프레그먼트인지 검사한다
+    if (child.type === React.Fragment) return;
+
+    // Route 컴포넌트인지 검사한다. 덕 타이핑
+    if (!child.props.path || !child.props.element) return;
+
+    // Route에 등록된 컴포넌트가 요청한 경로에 해당하는지 검사한다.
+    // 요청 경로에서 쿼리 문자열을 제거하고 비교한다.
+    if (child.props.path !== path.replace(/\?.*$/, "")) return;
+
+    // 엘리먼트를 찾았다.
+    selectedRoute = child.props.element;
+  });
+
+  return selectedRoute;
+};
+
+/**
+ * 사용 예
+ * <MyRouter.Route path={'경로'} element={<리액트_앨리먼트 />} />
+ */
+export const Route = () => null;
+
+export const Link = ({ to, ...rest }) => {
+  const { path, changePath } = React.useContext(routerContext);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (to !== path) changePath(to);
+  };
+
+  return <a {...rest} href={to} onClick={handleClick} />;
+};
