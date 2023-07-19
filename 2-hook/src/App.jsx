f47a102a8c45c04d9870543b1ebb1fb3c37738ef
김정환 <ej88ej@gmail.com>
2023-07-19 19:44:19 +0900

    OrderPage

--- 2-hook/src/App.jsx
+++ 2-hook/src/App.jsx
@@ -1,10 +1,12 @@
 import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
+import OrderPage from "./pages/OrderPage";
 
 const App = () => (
   <>
     {/* <ProductPage /> */}
-    <CartPage />
+    {/* <CartPage /> */}
+    <OrderPage />
   </>
 );
 
--- 2-hook/src/pages/OrderPage/index.jsx
+++ 2-hook/src/pages/OrderPage/index.jsx
@@ -1,3 +1,44 @@
-const OrderPage = () => <>OrderPage</>;
+import React from "react";
+import OrderApi from "shared/api/OrderApi";
+import Page from "../../components/Page";
+import Title from "../../components/Title";
+import Navbar from "../../components/Navbar";
+import OrderStatusCard from "./OrderStatusCard";
+import OrderPaymentCard from "./OrderPaymentCard";
+import OrderDeliveryCard from "./OrderDeliveryCard";
+
+const OrderPage = () => {
+  const [order, setOrder] = React.useState();
+
+  const fetch = async () => {
+    // TODO: startLoading("주문 정보 로딩중...");
+    try {
+      const order = await OrderApi.fetchMyOrder();
+      setOrder(order);
+    } catch (e) {
+      // TODO: openDialog(<ErrorDialog />);
+      return;
+    }
+    // TODO: finishLoading();
+  };
+
+  React.useEffect(() => {
+    fetch();
+  }, []);
+
+  return (
+    <div className="OrderPage">
+      <Page header={<Title>주문내역</Title>} footer={<Navbar />}>
+        {order && (
+          <>
+            <OrderStatusCard order={order} />
+            <OrderPaymentCard order={order} />
+            <OrderDeliveryCard order={order} />
+          </>
+        )}
+      </Page>
+    </div>
+  );
+};
 
 export default OrderPage;
