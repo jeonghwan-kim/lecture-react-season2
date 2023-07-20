ff0a1ed3a1a6c7fefee9a1a3f1efe49ec1378d5d
김정환 <ej88ej@gmail.com>
2023-07-21 08:21:22 +0900

    MyForm

---
+++ 2-hook/src/lib/MyForm.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+
+export const useForm = ({ initialValue, validate, onSubmit }) => {
+  const [values, setValues] = React.useState(initialValue);
+  const [errors, setErrors] = React.useState({});
+  const [touched, setTouched] = React.useState({});
+
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleBlur = (e) => {
+    setTouched({
+      ...touched,
+      [e.target.name]: true,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const nextTouched = Object.keys(values).reduce((touched, field) => {
+      touched[field] = true;
+      return touched;
+    }, {});
+    setTouched(nextTouched);
+
+    const errors = validate(values);
+    setErrors(errors);
+    if (Object.values(errors).some(Boolean)) return;
+
+    onSubmit(values);
+  };
+
+  const getFieldProps = (name) => {
+    const value = values[name];
+    const onBlur = handleBlur;
+    const onChange = handleChange;
+
+    return {
+      name,
+      value,
+      onBlur,
+      onChange,
+    };
+  };
+
+  React.useEffect(() => {
+    setErrors(validate(values));
+  }, [values]);
+
+  return {
+    values,
+    errors,
+    touched,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+    getFieldProps,
+  };
+};
+
+const formContext = React.createContext({});
+formContext.displayName = "FormContext";
+
+export const Form = ({ children, ...rest }) => {
+  const formValue = useForm(rest);
+  return (
+    <formContext.Provider value={formValue}>
+      <form noValidate onSubmit={formValue.handleSubmit}>
+        {children}
+      </form>
+    </formContext.Provider>
+  );
+};
+
+export const Field = ({ as = "input", children, ...rest }) => {
+  const { getFieldProps } = React.useContext(formContext);
+  return React.createElement(
+    as,
+    { ...rest, ...getFieldProps(rest.name) },
+    children
+  );
+};
+
+export const ErrorMessage = ({ name }) => {
+  const { touched, errors } = React.useContext(formContext);
+  if (!touched[name] || !errors[name]) return null;
+  return <span>{errors[name]}</span>;
+};
