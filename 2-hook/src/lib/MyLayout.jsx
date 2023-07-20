dd92d4779c005ce14197781855933ba34bf795a9
김정환 <ej88ej@gmail.com>
2023-07-20 09:22:15 +0900

    Layout

---
+++ 2-hook/src/lib/MyLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const layoutContext = React.createContext({});
+layoutContext.displayName = "LayoutContext";
+
+export const Layout = ({ children }) => {
+  const [dialog, setDialog] = React.useState();
+
+  return (
+    <layoutContext.Provider value={{ dialog, setDialog }}>
+      {children}
+    </layoutContext.Provider>
+  );
+};
