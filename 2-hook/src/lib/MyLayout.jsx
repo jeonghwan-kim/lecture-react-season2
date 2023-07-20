a8892dd06591efefba3897efc66b5829afd9d3d2
김정환 <ej88ej@gmail.com>
2023-07-20 09:29:49 +0900

    layout hooks

--- 2-hook/src/lib/MyLayout.jsx
+++ 2-hook/src/lib/MyLayout.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import Dialog from "../components/Dialog";
+import Backdrop from "../components/Backdrop";
 
 export const layoutContext = React.createContext({});
 layoutContext.displayName = "LayoutContext";
@@ -10,5 +13,31 @@ export const Layout = ({ children }) => {
     <layoutContext.Provider value={{ dialog, setDialog }}>
       {children}
     </layoutContext.Provider>
+  );
+};
+
+export const useDialog = () => {
+  const { dialog, setDialog } = React.useContext(layoutContext);
+  const openDialog = (element) => setDialog(element);
+  const closeDialog = () => setDialog(null);
+  return { dialog, openDialog, closeDialog };
+};
+
+export const useLoading = () => {
+  const { openDialog, closeDialog: finishLoading } = useDialog();
+  const startLoading = (message) => openDialog(<Dialog>{message}</Dialog>);
+  return { startLoading, finishLoading };
+};
+
+export const DialogContainer = () => {
+  const { dialog } = useDialog();
+  return (
+    <>
+      {dialog &&
+        ReactDOM.createPortal(
+          <Backdrop>{dialog}</Backdrop>,
+          document.querySelector("#dialog")
+        )}
+    </>
   );
 };
