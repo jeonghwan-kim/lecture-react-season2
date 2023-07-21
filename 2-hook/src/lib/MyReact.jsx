0e190e19c9252a2c9a230ebdbd9d956f26e0ba5a
김정환 <ej88ej@gmail.com>
2023-07-21 22:50:15 +0900

    useMemo

--- 2-hook/src/lib/MyReact.jsx
+++ 2-hook/src/lib/MyReact.jsx
@@ -154,6 +154,27 @@ const MyReact = (function MyReact() {
     return [store.getState(), store.dispatch];
   }
 
+  function useMemo(nextCreate, deps) {
+    if (!memorizedStates[cursor]) {
+      const nextValue = nextCreate();
+      memorizedStates[cursor] = [nextValue, deps];
+      cursor = cursor + 1;
+      return nextValue;
+    }
+
+    const nextDeps = deps;
+    const [prevValue, prevDeps] = memorizedStates[cursor];
+    if (prevDeps.every((prev, idx) => prev === nextDeps[idx])) {
+      cursor = cursor + 1;
+      return prevValue;
+    }
+
+    const nextValue = nextCreate();
+    memorizedStates[cursor] = [nextValue, nextDeps];
+    cursor = cursor + 1;
+    return nextValue;
+  }
+
   return {
     useState,
     useEffect,
@@ -161,6 +182,7 @@ const MyReact = (function MyReact() {
     useContext,
     useRef,
     useReducer,
+    useMemo,
 
     resetCursor,
     cleanupEffects,
